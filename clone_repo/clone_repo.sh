be014a2d5fb72280e26f5e2877b154fcf9a67beb
Grzegorz Baran <grzebar@agora.pl>
2024-02-23 08:21:38 +0100

    feat: clone repo with history

--- clone_repo/clone_repo.sh
+++ clone_repo/clone_repo.sh
@@ -13,19 +13,19 @@ GITLAB_REPO_URL="$1"
 BITBUCKET_REPO_URL="$2"
 
 # Clone GitLab repository
-git clone --mirror $GITLAB_REPO_URL temp_repo
+git clone --mirror "$GITLAB_REPO_URL" temp_repo
 
 # Change directory to cloned repository
-cd temp_repo
+cd temp_repo || exit
 
 # Push mirrored repository to Bitbucket
-git push --mirror $BITBUCKET_REPO_URL
+git push --mirror "$BITBUCKET_REPO_URL"
 
 # Push tags to Bitbucket
-git push --tags $BITBUCKET_REPO_URL
+git push --tags "$BITBUCKET_REPO_URL"
 
 # Clean up temporary repository
-cd ..
+cd .. || exit
 rm -rf temp_repo
 
 echo "Repository successfully migrated from GitLab to Bitbucket."
